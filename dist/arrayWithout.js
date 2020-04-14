de480f2eddc310296c7b0457968de162d1407452
Joeri Bakker <bakkerjoeri@gmail.com>
2020-04-14 14:29:11 +0200

    2.0.0

--- dist/arrayWithout.js
+++ dist/arrayWithout.js
@@ -1,12 +1,5 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function arrayWithout(array) {
-    var valuesToExclude = [];
-    for (var _i = 1; _i < arguments.length; _i++) {
-        valuesToExclude[_i - 1] = arguments[_i];
-    }
-    return array.filter(function (value) {
+export default function arrayWithout(array, ...valuesToExclude) {
+    return array.filter((value) => {
         return !valuesToExclude.includes(value);
     });
 }
-exports.default = arrayWithout;
